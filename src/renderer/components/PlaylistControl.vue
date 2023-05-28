cad115d66c6fd25e0652f3502e699ef55a2ed0f1
huangxulei <pjhxl@qq.com>
2023-05-28 13:48:49 +0800

    52811

--- src/renderer/components/PlaylistControl.vue
+++ src/renderer/components/PlaylistControl.vue
@@ -1,16 +1,22 @@
 <script setup>
 import { useRouter } from 'vue-router';
 import PaginationTiles from './PaginationTiles.vue';   
+import ImageTextTile from './ImageTextTile.vue';
 const props = defineProps({
     data: Array
 })    
+
+const router = useRouter()
 </script>
         
 <template>
     <div class="playlists-ctl">
         <PaginationTiles>
           <template #data>
-           111
+           <ImageTextTile v-for="item in data"
+           :cover="item.cover"
+           :title="item.title">
+           </ImageTextTile>
         </template>  
         </PaginationTiles>
     </div>
--- src/renderer/views/SquareView.vue
+++ src/renderer/views/SquareView.vue
@@ -4,6 +4,7 @@ import { storeToRefs } from "pinia";
 import EventBus from "../../common/EventBus";
 import { useSquareViewStore } from "../store/squareViewStore";
 import CategoryBar from "../components/CategoryBar.vue";
+import PlaylistControl from "../components/PlaylistControl.vue";
 const { currentPlatformCode, currentCategoryCode } = storeToRefs(
   useSquareViewStore()
 );
@@ -26,59 +27,64 @@ const resetPagination = () => {
   pagination.page = 1;
 };
 
-const nextPage = ()=>{
-  pagination.offset = pagination.page * pagination.limit
-  pagination.page = pagination.page + 1
-}
+const nextPage = () => {
+  pagination.offset = pagination.page * pagination.limit;
+  pagination.page = pagination.page + 1;
+};
 
 const loadCategoyies = () => {
-  categories.length = 0
-  let cached = currentCategory()
-  if(!cached){
-    const vender = currentVender()
-    if(!vender) return
+  categories.length = 0;
+  let cached = currentCategory();
+  if (!cached) {
+    const vender = currentVender();
+    if (!vender) return;
     vender.categories().then((result) => {
-      putCurrentCategory(result) 
-      categories.push(...result)
+      putCurrentCategory(result);
+      categories.push(...result);
       //EventBus.emit('category-update')
-    })
-  }else {
-    categories.push(...cached)
+    });
+  } else {
+    categories.push(...cached);
     //EventBus.emit('category-update')
   }
-}
+};
 
 const loadContent = () => {
-  const vender = currentVender()
-  if(!vender) return
-  const cate = currentCategoryCode.value
-  const offset = pagination.offset
-  const limit = pagination.limit
-  const page = pagination.page
-  vender.square(cate, offset, limit, page).then(result =>{
-    console.log(result)
-    playlists.push(...result.data)
-  })
-}
+  const vender = currentVender();
+  if (!vender) return;
+  const cate = currentCategoryCode.value;
+  const offset = pagination.offset;
+  const limit = pagination.limit;
+  const page = pagination.page;
+  vender.square(cate, offset, limit, page).then((result) => {
+    console.log(result);
+    playlists.push(...result.data);
+  });
+};
 
 const loadData = () => {
-  loadCategoyies()
-  loadContent()
-}
-onMounted(() => { 
-  resetPagination()
-  loadData()
+  loadCategoyies();
+  loadContent();
+};
+onMounted(() => {
+  resetPagination();
+  loadData();
 });
 
-watch(currentPlatformCode, (nv, ov) => {
-  playlists.length = 0
-
-})
+watch(currentCategoryCode, (nv, ov) => {
+  playlists.length = 0;
+  resetPagination();
+  loadContent();
+});
 </script>
 
 <template>
   <div id="square-content" ref="SquareContentRef">
-    <CategoryBar :data="categories" v-show="categories.length > 0"></CategoryBar>
+    <CategoryBar
+      :data="categories"
+      v-show="categories.length > 0"
+    ></CategoryBar>
+    <PlaylistControl :data="playlists"></PlaylistControl>
   </div>
 </template>
 
